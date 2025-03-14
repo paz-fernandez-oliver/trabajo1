eb714a7b616fbd0d698a15b85e195b97a8349887
Paz Fernandez Oliver <pazfernandezoliver@gmail.com>
2025-03-14 18:53:01 -0300

    prueba uno

---
+++ tp1.py
@@ -0,0 +1,15 @@
+"""Desarrolla un programa que solicite al usuario que ingrese su edad y luego
+ calcule y muestre cuántos años le faltan para alcanzar los 100 años"""
+
+"""edad=int(input("ingrese su edad"))
+print (100-edad)"""
+
+"""Haz un programa que pida al usuario que ingrese una temperatura en
+ grados Celsius y luego convierta esa temperatura a grados Fahrenheit,
+ mostrando el resultado."""
+
+temperatura=int(input("ingresa la tempratura en Celsius"))
+NuevaTemp=0 
+NuevaTemp=(temperatura * 1.8)+ 32
+print (NuevaTemp)
+
